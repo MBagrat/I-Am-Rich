00f8301cb289115c314e861422533a575777e9da
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2022-09-12 13:58:09 +0400

    I Am Rich

---
+++ Shared/i_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_richApp.swift
+//  Shared
+//
+//  Created by Bagrat Mukaelyan on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct i_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

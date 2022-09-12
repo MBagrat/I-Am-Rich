a78ac3a13af6f762acacf53ae1f197490efd1a24
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2022-09-12 14:01:44 +0400

    Update changes

---
+++ I Am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am rich
+//
+//  Created by Bagrat Mukaelyan on 12.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
